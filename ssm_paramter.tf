ed84fde07a68ff9e01c78a0175be433aee44cc5a
Nicolas Abihaggle <rabihaggle@gmail.com>
2023-09-03 11:25:11 +0200

    add ssm_parameters

---
+++ ssm_paramter.tf
@@ -0,0 +1,17 @@
+resource "aws_ssm_parameter" "foo_v1" {
+  name  = "/lab/service/ssm_parameter_ignorechanges"
+  type  = "String"
+  value = "esto_es_una variable"
+
+lifecycle {
+  ignore_changes = [ value ]
+}
+
+}
+
+resource "aws_ssm_parameter" "foo_v2" {
+  name  = "/lab/service/ssm_parameter"
+  type  = "String"
+  value = "esto_es_una variable"
+}
+
